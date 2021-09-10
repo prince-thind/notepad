41b9763618d39dcb750a3f83f19a2eebda752ac6
Prince-Thind <princethind799@gmail.com>
2021-09-10 14:18:33 +0530

    nit: fix formatting

--- src/index.js
+++ src/index.js
@@ -6,24 +6,24 @@ const editForm = document.querySelector('#edit-form');
 saveBtn.addEventListener('click', saveText);
 fileUploadForm.addEventListener('submit', uploadText);
 textArea.addEventListener('change', updateLocalStorage);
-editForm.addEventListener('submit',edit);
+editForm.addEventListener('submit', edit);
 init();
 
-function edit(e){
-    e.preventDefault();
-    const fontSize=e.target['font-size'].value;
-    textArea.style.fontSize=fontSize+'px';
-    localStorage.setItem('notepad-font-size',fontSize)
+function edit(e) {
+  e.preventDefault();
+  const fontSize = e.target['font-size'].value;
+  textArea.style.fontSize = fontSize + 'px';
+  localStorage.setItem('notepad-font-size', fontSize);
 }
-function updateLocalStorage(e){
-    const text=e.target.value;
-    localStorage.setItem('notepad-text',text);
+function updateLocalStorage(e) {
+  const text = e.target.value;
+  localStorage.setItem('notepad-text', text);
 }
 
-function init(){
-    textArea.value=localStorage.getItem('notepad-text')
-    textArea.style.fontSize=(localStorage.getItem('notepad-font-size')||'16')+'px';
-    
+function init() {
+  textArea.value = localStorage.getItem('notepad-text');
+  textArea.style.fontSize =
+    (localStorage.getItem('notepad-font-size') || '16') + 'px';
 }
 
 function saveText(e) {
@@ -42,9 +42,9 @@ function uploadText(e) {
   e.preventDefault();
   const inputFile = e.target.file.files[0];
   const textPromise = readText(inputFile);
-  textPromise.then(text=>{
-      textArea.value=text;
-  })
+  textPromise.then((text) => {
+    textArea.value = text;
+  });
 }
 
 async function readText(file) {
