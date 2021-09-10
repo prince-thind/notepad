d75ce5eb463c6c1042c209c104ca717205e76e1c
Prince-Thind <princethind799@gmail.com>
2021-09-10 13:56:02 +0530

    add file upload and local storage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-const saveBtn=document.querySelector('#save-text');
-const textArea=document.querySelector('#text');
-saveBtn.addEventListener('click',saveText);
+const saveBtn = document.querySelector('#save-text');
+const textArea = document.querySelector('#text');
+const fileUploadForm = document.querySelector('#file-upload-form');
+saveBtn.addEventListener('click', saveText);
+fileUploadForm.addEventListener('submit', uploadText);
+textArea.addEventListener('change', updateLocalStorage);
+init();
 
-function saveText(e){
-    e.preventDefault();
-    const text=textArea.value;
-    const downloadBtn=document.createElement('a');
-    downloadBtn.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-    downloadBtn.setAttribute('download', 'text.txt');
-    downloadBtn.click();
+function updateLocalStorage(e){
+    const text=e.target.value;
+    localStorage.setItem('notepad-text',text);
+}
+
+function init(){
+    textArea.value=localStorage.getItem('notepad-text')
+}
+
+function saveText(e) {
+  e.preventDefault();
+  const text = textArea.value;
+  const downloadBtn = document.createElement('a');
+  downloadBtn.setAttribute(
+    'href',
+    'data:text/plain;charset=utf-8,' + encodeURIComponent(text)
+  );
+  downloadBtn.setAttribute('download', 'text.txt');
+  downloadBtn.click();
+}
+
+function uploadText(e) {
+  e.preventDefault();
+  const inputFile = e.target.file.files[0];
+  const textPromise = readText(inputFile);
+  textPromise.then(text=>{
+      textArea.value=text;
+  })
+}
+
+async function readText(file) {
+  const text = await new Response(file).text();
+  return text;
 }
